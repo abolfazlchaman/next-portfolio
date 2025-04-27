22b75b3e413c4f3155ee2de40234f659eb5b7575
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-04-27 19:26:30 +0330

    added json-ld based on langauage for SEO

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { getDictionary } from "../../get-dictionary";
 import { Locale } from "../../i18n-config";
 import { About } from "./components/about";
@@ -16,56 +17,103 @@ export default async function IndexPage(props: { params: Promise<{ lang: Locale 
     developerInfo: { fullName },
   } = dictionary;
 
+  const isEnglish = lang === "en";
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Person",
+    name: isEnglish ? "Abolfazl Chaman" : "ابوالفضل چمن",
+    image: "https://www.abolfazlchaman.ir/images/AbolfazlChamanFormal.jpg",
+    description: isEnglish
+      ? "Discover the portfolio of Abolfazl Chaman, a seasoned Web Developer and Software Engineer with over 5 years of experience delivering high-performance, scalable web applications. Specializing in React, Next.js, TypeScript, and modern full-stack web and software development. Check out my resume and let's discuss how I can help improve your team or project output."
+      : "پورتفولیوی ابوالفضل چمن، توسعه‌دهنده وب و مهندس نرم‌افزار با بیش از ۵ سال تجربه در ارائه اپلیکیشن‌های وب با عملکرد بالا و مقیاس‌پذیر. متخصص در ری‌اکت، نکست‌جی‌اس، تایپ‌اسکریپت و توسعه وب و نرم‌افزار فول‌استک مدرن. رزومه من را مشاهده کنید و با من در مورد بهبود عملکرد تیم یا خروجی پروژه خود ارتباط برقرار کنید.",
+    url: isEnglish ? "https://www.abolfazlchaman.ir/en" : "https://www.abolfazlchaman.ir/fa",
+    sameAs: [
+      "https://www.linkedin.com/in/abolfazlchaman",
+      "https://www.github.com/abolfazlchaman",
+      "https://t.me/+989027001688",
+      "https://t.me/+989352121688",
+      "https://wa.me/+989027001688",
+      "https://wa.me/+989352121688",
+      "mailto:info.abolfazlchaman@gmail.com",
+      "mailto:abolfazlchaman.info@gmail.com",
+      "mailto:abolfazl.chaman.3@gmail.com",
+    ],
+    worksFor: [
+      {
+        "@type": "Organization",
+        "name": "Meta",
+        "url": "https://www.meta.com",
+        "roleName": "Open Source Contributor",
+        "sameAs": "https://github.com/abolfazlchaman",
+      },
+      {
+        "@type": "Organization",
+        "name": "Airbnb",
+        "url": "https://www.airbnb.com",
+        "roleName": "Open Source Contributor",
+        "sameAs": "https://github.com/abolfazlchaman",
+      },
+    ],
+  };
+
   return (
-    <main>
-      <Navigation
-        dictionary={dictionary.navigation}
-        fullName={fullName}
-        theme={dictionary.theme}
-      />
-      <div className="container md:px-40 xl:px-60 px-10 min-w-full">
-        <section
-          id="hero"
-          aria-label="Main hero section">
-          <Hero dictionary={dictionary} />
-        </section>
-        <section
-          id="about"
-          aria-label="About Abolfazl Chaman section">
-          <About
-            dictionary={dictionary.about}
-            language={lang}
-          />
-        </section>
-        <section
-          id="skills"
-          className="scroll-mt-20">
-          <Skills
-            skillsTitles={dictionary.skills}
-            softSkills={dictionary.softSkills}
-            languages={dictionary.languages}
-          />
-        </section>
-        <section
-          id="projects"
-          className="scroll-mt-20">
-          <Projects projects={dictionary.projects} />
-        </section>
-        <section
-          id="inspiration"
-          className="scroll-mt-20">
-          <Inspirations
-            inspiration={dictionary.inspiration}
-            quotes={dictionary.quotes}
-          />
-        </section>
-      </div>
-      <Footer
-        language={lang}
-        footer={dictionary.footer}
-        fullName={fullName}
-        socialLinks={dictionary.socialLinks}
-      />
-    </main>
+    <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </Head>
+      <main>
+        <Navigation
+          dictionary={dictionary.navigation}
+          fullName={fullName}
+          theme={dictionary.theme}
+        />
+        <div className="container md:px-40 xl:px-60 px-10 min-w-full">
+          <section
+            id="hero"
+            aria-label="Main hero section">
+            <Hero dictionary={dictionary} />
+          </section>
+          <section
+            id="about"
+            aria-label="About Abolfazl Chaman section">
+            <About
+              dictionary={dictionary.about}
+              language={lang}
+            />
+          </section>
+          <section
+            id="skills"
+            className="scroll-mt-20">
+            <Skills
+              skillsTitles={dictionary.skills}
+              softSkills={dictionary.softSkills}
+              languages={dictionary.languages}
+            />
+          </section>
+          <section
+            id="projects"
+            className="scroll-mt-20">
+            <Projects projects={dictionary.projects} />
+          </section>
+          <section
+            id="inspiration"
+            className="scroll-mt-20">
+            <Inspirations
+              inspiration={dictionary.inspiration}
+              quotes={dictionary.quotes}
+            />
+          </section>
+        </div>
+        <Footer
+          language={lang}
+          footer={dictionary.footer}
+          fullName={fullName}
+          socialLinks={dictionary.socialLinks}
+        />
+      </main>
+    </>
   );
 }
