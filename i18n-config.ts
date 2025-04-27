d65b78f96315b9cca65a60fa320cba7ba245f525
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-04-27 06:40:42 +0330

    label fixes

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,6 +1,10 @@
 export const i18n = {
   defaultLocale: 'en',
-  locales: ['en', 'fa']
+  locales: ['en', 'fa'],
+  labels: {
+    en: 'English',
+    fa: 'فارسی'
+  }
 } as const
 
 export type Locale = typeof i18n['locales'][number]
