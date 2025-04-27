6734e05b1e36461956cd29de16649dbb865bb241
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-04-27 06:04:19 +0330

    ts config and some types added

---
+++ types/types.d.ts
@@ -0,0 +1,69 @@
+// types/translation.d.ts
+export interface Quote {
+  quote: string
+  author: string
+  wiki: string
+}
+
+export interface TranslationDict {
+  language: string
+  developerInfo: {
+    fullName: string
+    profession: string
+    experience: string
+    location: string
+  }
+  socialLinks: { platform: string; url: string }[]
+  theme: {
+    toggle: string
+    light: string
+    dark: string
+    system: string
+  }
+  quotes: Quote[]
+  navigation: {
+    home: string
+    about: string
+    projects: string
+    blog: string
+    toggle_menu: string
+  }
+  footer: {
+    quote: string
+    author: string
+    copyrightText: string
+  }
+  about: {
+    title: string
+    description: string
+    buttonTextFa: string
+    buttonTextEn: string
+  }
+  projects: {
+    title: string
+    subtitle: string
+    projectsData: {
+      title: string
+      description: string
+      link: string
+      technologies: string[]
+    }[]
+  }
+  inspiration: {
+    description: string
+    seeAllInspirations: string
+  }
+}
+export type NavLinksType = {
+  navigation: {
+    home: string
+    about: string
+    skills: string
+    projects: string
+    inspirations: string
+    // blog?: string;
+  }
+  developerInfo: {
+    fullName: string
+  }
+}
